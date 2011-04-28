dc691c7b9f9bae75c55ff4d3f317dd17704138f8
cpscotti <scotti@ieee.org>
2011-04-28 11:57:14 +0100

    Adding NPushRotationTick that carries the rotation info.

---
+++ PushBurton2/npushrotationstick.h
@@ -0,0 +1,34 @@
+#ifndef ROTATIONSTICK_H
+#define ROTATIONSTICK_H
+
+#include <QtXml>
+#include <QString>
+#include <QObject>
+
+#include "npushlogtick.h"
+#include "math.h"
+
+class NPushRotationsTick : public NPushLogTick
+{
+public:
+    NPushRotationsTick(double a_rawDegs, quint64 a_msecsToEpoch);
+
+    ~NPushRotationsTick();
+
+    void append_to_xml(QDomDocument& doc, QDomElement& root) const;
+    void dump_to_xml(QXmlStreamWriter& xml) const;
+    QString get_pretty_print() const;
+
+
+public:
+    quint64 msecsToEpoch;
+
+    double rawDegs;
+    double cleanDegs;
+
+private:
+    void updateCleanDegs();
+
+};
+
+#endif // ROTATIONSTICK_H
